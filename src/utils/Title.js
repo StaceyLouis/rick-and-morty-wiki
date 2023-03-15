d578c086c6ccfe01e1f90b2b6bdad3d7888b9155
Stacey Louis <staceylouis7@gmail.com>
2023-03-14 20:10:59 -0400

    glowing text title added

---
+++ src/utils/Title.js
@@ -0,0 +1,14 @@
+import '../App.css'
+import styled from 'styled-components'
+
+
+
+const Title = ({title}) => {
+ 
+  return (
+    <h1 className='title'>{title}</h1>
+ 
+  )
+}
+
+export default Title
